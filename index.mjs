07288c9e99e17a73231e49f14f8a2241117c8b61
(none) <noreply@github.com>
2020-03-31 18:28:15 -0700

    Update colors

--- index.mjs
+++ index.mjs
@@ -15,10 +15,10 @@ const gameHeight = height/zoom;
 const appleNum = Math.ceil(0.01 * gameWidth * gameHeight);
 const render = renderFactory(context, width, height, zoom);
 const playerConfig = [
-  {x: 0,                 y: 0,                velocity:+1,  horizontal:true,    brains: [new GamePadBrain(0, xbox), new KeyBrain({...arrows})],           color:'green' },
-  {x: 0,                 y: gameHeight - 1,   velocity:-1,  horizontal:false,   brains: [new GamePadBrain(0, antiXbox), new KeyBrain({...antiArrows})],   color:'yellow'},
-  {x: gameWidth - 1,     y: 0,                velocity:+1,  horizontal:false,   brains: [new GamePadBrain(1, xbox), new KeyBrain({...wasd})],             color:'blue'  },
-  {x: gameWidth - 1,     y: gameHeight - 1,   velocity:-1,  horizontal:true,    brains: [new GamePadBrain(1, antiXbox), new KeyBrain({...antiWasd})],     color:'purple'},
+  {x: 0,                 y: 0,                velocity:+1,  horizontal:true,    brains: [new GamePadBrain(0, xbox), new KeyBrain({...arrows})],           color:'#4e9a06' },
+  {x: 0,                 y: gameHeight - 1,   velocity:-1,  horizontal:false,   brains: [new GamePadBrain(0, antiXbox), new KeyBrain({...antiArrows})],   color:'#8ae232'},
+  {x: gameWidth - 1,     y: 0,                velocity:+1,  horizontal:false,   brains: [new GamePadBrain(1, xbox), new KeyBrain({...wasd})],             color:'#c4a000'  },
+  {x: gameWidth - 1,     y: gameHeight - 1,   velocity:-1,  horizontal:true,    brains: [new GamePadBrain(1, antiXbox), new KeyBrain({...antiWasd})],     color:'#fce94f'},
   {x: Math.floor(gameWidth/2),     y: Math.ceil(gameHeight/2),   velocity:Math.random() < 0.5 ? 1 : -1,  horizontal: Math.random() < 0.5 ? true : false,    brains: [new RandomBrain({'clockwise':1,'counterclockwise':1, '':2}, 500)],     color:'white'},
 
 ];
